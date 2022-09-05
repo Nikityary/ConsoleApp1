9abf3caa5361fa5ec7ab2237cb2008c902c40fcb
Nikityara <Nikityara@KOMPUTER>
2022-09-05 18:36:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char[] a = { 'ё', 'у', 'е', 'ы', 'а', 'а', 'о', 'э', 'я', 'и', 'ю' };
+        Console.WriteLine(Console.ReadLine().ToCharArray().Count(b => a.Contains(b)));
+    }
+}
